ccbe5f3d7f4e03e42f6f68e157b5a8d18c467473
Ethan Budd <ethan@ebudd.io>
2020-09-03 14:58:58 -0500

    Updates

---
+++ precache-manifest.274532843a100254268e7f6456bab4a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "246bc169e08c676e8693da84b289410b",
+    "url": "/portalU/index.html"
+  },
+  {
+    "revision": "393e00432f54f4abeb9d",
+    "url": "/portalU/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6f89725780ebecd51993",
+    "url": "/portalU/static/css/main.f76c45a0.chunk.css"
+  },
+  {
+    "revision": "393e00432f54f4abeb9d",
+    "url": "/portalU/static/js/2.be3cf5de.chunk.js"
+  },
+  {
+    "revision": "594f8b3fb1fe0b6a3d9abea13b31ee07",
+    "url": "/portalU/static/js/2.be3cf5de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f89725780ebecd51993",
+    "url": "/portalU/static/js/main.c86d1b54.chunk.js"
+  },
+  {
+    "revision": "7bd5286acd8bf1a26abf",
+    "url": "/portalU/static/js/runtime-main.af9f1ce8.js"
+  }
+]);
