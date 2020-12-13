4c35a600ce899824298953ef8475424f52664519
budde25 <budde25@protonmail.com>
2020-12-13 13:51:31 -0600

    Updates

---
+++ precache-manifest.aafc34a82588f5f089c2c9a97d09f005.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc53fccfadbf0faf96515825a87a2ebf",
+    "url": "/portalU/index.html"
+  },
+  {
+    "revision": "34e4e9b6640c194fbbfc",
+    "url": "/portalU/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "2b363a6766886ef7b9a0",
+    "url": "/portalU/static/css/main.f76c45a0.chunk.css"
+  },
+  {
+    "revision": "34e4e9b6640c194fbbfc",
+    "url": "/portalU/static/js/2.32eeecfc.chunk.js"
+  },
+  {
+    "revision": "74167855e9509a90530c0e303b7042bf",
+    "url": "/portalU/static/js/2.32eeecfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b363a6766886ef7b9a0",
+    "url": "/portalU/static/js/main.d8e22518.chunk.js"
+  },
+  {
+    "revision": "7bd5286acd8bf1a26abf",
+    "url": "/portalU/static/js/runtime-main.af9f1ce8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portalU/precache-manifest.d001f4ef6cae228187980192271ac4ac.js"
+  "/portalU/precache-manifest.aafc34a82588f5f089c2c9a97d09f005.js"
 );
 
 self.addEventListener('message', (event) => {
